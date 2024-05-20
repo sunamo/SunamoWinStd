067b1f74ceac8b828d99921f296fdd571ad3a729
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-20 06:51:24 +0200

    Before trying call automatic to fix remain errors

--- _sunamo/SHGetLines.cs
+++ _sunamo/SHGetLines.cs
@@ -3,6 +3,6 @@ internal class SHGetLines
 {
     internal static List<string> GetLines(string v)
     {
-        return vv.Split(new string[] { v.Contains("\r\n") ? "\r\n" : "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        return v.Split(new string[] { v.Contains("\r\n") ? "\r\n" : "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
 }
