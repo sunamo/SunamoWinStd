3efd69b3fb153dca0fe0b06b139d8bf114d94b54
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 11:37:52 +0200

    Make some classes public when it is needed

---
+++ _sunamo/SunamoArgs/ChangeContentArgsWinStd.cs
@@ -0,0 +1,9 @@
+namespace SunamoWinStd;
+
+
+internal class ChangeContentArgsWinStd
+{
+    internal bool removeNull = false;
+    internal bool removeEmpty = false;
+    internal bool switchFirstAndSecondArg = false;
+}
