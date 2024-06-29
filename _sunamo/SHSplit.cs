0eac6bc2e5dc8e9041383c18e5986b4d9f6ca5b6
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 07:56:42 +0200

    moved all in _sunamo to right subfolders by original project

--- _sunamo/SHSplit.cs
+++
@@ -1,8 +0,0 @@
-namespace SunamoWinStd;
-internal class SHSplit
-{
-    internal static List<string> SplitByWhiteSpaces(string innerText)
-    {
-        return innerText.Split(AllChars.whiteSpacesChars.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
-    }
-}
