87159c2110b777b10bb28841f6bed91ba0c12f42
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 18:36:51 +0200

    All buildable, before removing dead code

---
+++ _public/SunamoEnums/Enums/Browsers.cs
@@ -0,0 +1,38 @@
+namespace SunamoWinStd;
+
+
+
+
+
+public enum Browsers : byte
+{
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    Chrome = 1,
+    Firefox = 2,
+    EdgeBeta = 3,
+    Opera = 4,
+    Vivaldi = 5,
+    
+    Iridium = 6, 
+    Falkon = 7, 
+    OperaGX = 8, 
+    ChromeCanary = 9,
+    Tor = 10,
+    Bravebrowser = 11,
+    ChromeBeta = 12,
+    EdgeDev = 13,
+    EdgeCanary = 14,
+    ChromeDev = 15,
+    EdgeStable = 254,
+    None = 255
+}
